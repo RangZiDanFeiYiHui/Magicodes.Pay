eaade77c9bba129e30e2e8d84805dbe3d9540660
Kevin <kevin100702@hotmail.com>
2022-03-30 16:18:17 +0800

    https windows 提示错误

--- src/Magicodes.Pay.Wxpay/Helper/RequestUtility.cs
+++ src/Magicodes.Pay.Wxpay/Helper/RequestUtility.cs
@@ -144,6 +144,8 @@ namespace Magicodes.Pay.Wxpay.Helper
         /// <returns></returns>
         public static string HttpGet(string url, Encoding encoding = null)
         {
+            ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             var wc = new WebClient
             {
                 Proxy = _webproxy,
@@ -615,5 +617,23 @@ namespace Magicodes.Pay.Wxpay.Helper
         }
 
         #endregion
+
+        /// <summary>
+        /// Certificate validation callback.
+        /// </summary>
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors error)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (error == System.Net.Security.SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            Console.WriteLine("X509Certificate [{0}] Policy Error: '{1}'",
+                cert.Subject,
+                error.ToString());
+
+            return false;
+        }
     }
 }
